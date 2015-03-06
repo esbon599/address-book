e16ab592ce10703c28e80e13943a3b50c80676c2
Tom Mertz <tom.mertz@verizon.net>
2015-03-06 12:44:38 -0800

    Fix delete name confirmation

--- app/app.php
+++ app/app.php
@@ -12,14 +12,12 @@
     $_SESSION['contacts'] = array();
   }
 
-  // $_SESSION['contacts'] = array();
-
 
   $app = new Silex\Application();
   $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views/'));
 
   $app->get('/', function() use ($app) {
-    var_dump(array_keys(Contact::getContacts()));
+
     return $app['twig']->render('homepage.twig', array('contacts' => Contact::getContacts()));
   });
 
@@ -28,49 +26,47 @@
     //create a variable in case there is an error
     $error = "";
 
-    //check if we are coming from the delete page
-    if($_POST['no'] == "no")
-    {
-      echo "NOT DELETED";
-    }
-    //check if we have valid post variables
-    elseif($_POST['name'] && $_POST['phone'] && $_POST['address']) {
-      echo "Valid variables";
+    //check if we have valid post variables and we aren't coming from a delete menu
+    if($_POST['name'] && $_POST['phone'] && $_POST['address'] && $_POST['no'] != "no") {
+
       //create a contact object
       $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
       //save the object into the session
       $contact->save();
     } else {
+      //if we don't have valid input, tell the user
       $error = "Please fill out all fields!";
-      echo "invalid variables";
     }
-
-    var_dump($_SESSION['contacts']);
-
 
     return $app['twig']->render('homepage.twig', array('contacts' => Contact::getContacts(), 'error' => $error));
   });
 
   $app->post('/delete', function() use ($app) {
 
-    var_dump($_POST);
-
+    //created the deleted variables
     $deleted_name = "";
     $deleted_phone = "";
     $deleted_address = "";
+    $button = $_POST['button']; //check which page we should be on
 
     //if we are coming from the homepage
-    if($_POST['button'] == "homepage") {
+    if($button == "homepage") {
 
+      //get the variables from homepage.twig
       $deleted_name = $_POST['name'];
       $deleted_phone = $_POST['phone'];
       $deleted_address = $_POST['address'];
 
       //save the info to be deleted into the session
       $_SESSION['delete'] = array($deleted_name, $deleted_phone, $deleted_address);
-      echo "added a contact to delete";
-      var_dump($_SESSION['delete']);
+
+      //if we are coming from the "YES" delete button clicked
     } else {
+
+      //get our info that was saved in the session
+      $deleted_name = $_SESSION['delete'][0];
+      $deleted_phone = $_SESSION['delete'][1];
+      $deleted_address = $_SESSION['delete'][2];
 
       //loop through out stored contacts and see if any match the one we want to delete
       foreach($_SESSION['contacts'] as $key => $contact) {
@@ -79,15 +75,10 @@
         if($_SESSION['delete'][0] == $contact->getName() && $_SESSION['delete'][1] == $contact->getPhone() && $_SESSION['delete'][2] == $contact->getAddress()) {
           unset($_SESSION['contacts'][$key]);
         }
-        $count++;
       }
-      echo $count;
-
-      var_dump($_SESSION['contacts']);
     }
 
-
-    return $app['twig']->render('delete.twig', array('phone' => $deleted_phone, 'name' => $deleted_name, 'address' => $deleted_address));
+    return $app['twig']->render('delete.twig', array('phone' => $deleted_phone, 'name' => $deleted_name, 'address' => $deleted_address, 'button' => $button));
   });
 
   $app->post('/deleted', function() use ($app) {
