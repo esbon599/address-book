e545f5f32de31bba49735848bc7e8ed2a35cb6a1
Tom Mertz <tom.mertz@verizon.net>
2015-03-06 09:56:30 -0800

    Add session storage

--- app/app.php
+++ app/app.php
@@ -6,6 +6,12 @@
   //start cookie session for browser
   session_start();
 
+  //if our session array is emtpy
+  if(empty($_SESSION['contacts']))
+  {
+    $_SESSION['contacts'] = array();
+  }
+
 
   $app = new Silex\Application();
   $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views/'));
@@ -15,7 +21,20 @@
   });
 
   $app->post('/', function() use ($app) {
-    
+
+    //check if we have valid post variables
+    if($_POST['name'] && $_POST['phone'] && $_POST['address']) {
+      echo "Valid variables";
+      //create a contact object
+      $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
+      //save the object into the session
+      $contact->save();
+    } else {
+      echo "invalid variables";
+    }
+
+    var_dump($_SESSION['contacts']);
+
 
     return $app['twig']->render('homepage.twig');
   });
