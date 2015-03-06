3695d9d19e417b31e2515b8d9efe08cdf8711ee9
Tom Mertz <tom.mertz@verizon.net>
2015-03-06 09:45:55 -0800

    Add post barebones

--- app/app.php
+++ app/app.php
@@ -3,10 +3,20 @@
   require_once __DIR__.'/../vendor/autoload.php';
   require_once __DIR__.'/../src/contact.php';
 
+  //start cookie session for browser
+  session_start();
+
+
   $app = new Silex\Application();
   $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views/'));
 
-  $app->get('/', function () use ($app) {
+  $app->get('/', function() use ($app) {
+    return $app['twig']->render('homepage.twig');
+  });
+
+  $app->post('/', function() use ($app) {
+    
+
     return $app['twig']->render('homepage.twig');
   });
 
