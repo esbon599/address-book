e1ab311a36c5e32907068c9febc3a3ba145949c2
Tom Mertz <tom.mertz@verizon.net>
2015-03-06 09:34:14 -0800

    Add twig functionality

--- app/app.php
+++ app/app.php
@@ -1,10 +1,11 @@
 <?php
 
   require_once __DIR__.'/../vendor/autoload.php';
+  require_once __DIR__.'/../src/contact.php';
 
   $app = new Silex\Application();
 
-  $app->get('/', function () {
+  $app->get('/', function () use ($app) {
     return "HOME";
   });
 
